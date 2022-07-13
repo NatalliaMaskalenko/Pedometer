4a11dbc5cac23a161b289e5dd5a8a3de9a28852c
NatalliaMaskalenko <MoskNatulya@gmail.com>
2022-07-13 02:22:36 +0200

    Добавляет модалку

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,12 +1,32 @@
 (() => {
   const refs = {
-    modal: document.querySelector('[data-modal]')    
+    modal: document.querySelector('[data-modal]'),
+    wrapp: document.querySelector('[data-wrapp]')  
   };
 
   refs.modal.addEventListener('click', closeModal);  
+  document.addEventListener("keydown", closeModalKeydown);
 
-  function closeModal() {
+  function closeModal(event) {
+    event.preventDefault();
+    
+    if (event.target.id !== 'backdrop') {
+      return
+    }
+      
     refs.modal.classList.toggle('is-hidden');
+    refs.wrapp.classList.toggle('is-open');
+  }
+
+  function closeModalKeydown(event) {
+    event.preventDefault();
+    
+    if (event.key !== 'Escape') {
+      return
+    }
+      
+    refs.modal.classList.toggle('is-hidden');
+    refs.wrapp.classList.toggle('is-open');
   }
 
 })();
